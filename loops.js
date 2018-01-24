6f22a964ca4ccb544988ecbdd60bd7696b462138
THINH NGUYEN <thinhnguyen74@gmail.com>
2018-01-24 18:03:56 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,15 @@
+function forLoop(Array){
+  for (let i = 0; i < 25; i++) {
+    if(i===1){
+      Array.push('I am 1 strange loop.')
+    }
+    else {Array.push(`I am ${i} strange loops.`)}
+  } return Array
+}
+
+function whileLoop(n){
+  while (n > 0) {
+    console.log(n)
+  }
+  console.log("done")
+}
